61117198aff27fc18da59080108bc057fe318620
augustsletto <augustsletto@gmail.com>
2024-10-27 23:00:26 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from turtle import Screen, Turtle
+
+
+screen = Screen()
+
+
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong")
+
+
+paddle = Turtle()
+
+paddle.shape("square")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
