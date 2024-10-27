35c30ff7d274f4dee49114e47595603f17ac92f9
augustsletto <augustsletto@gmail.com>
2024-10-27 23:41:07 +0100

    Ball bounces on walls and tiles

--- ball.py
+++ ball.py
@@ -18,6 +18,11 @@ class Ball(Turtle):
         self.goto(new_x, new_y)
         
         
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
+        
+    
+    def bounce_x(self):
+        self.x_move *= -1
+        
         
